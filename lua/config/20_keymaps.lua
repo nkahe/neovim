5b956a5ceba8135120ae816dfff405c19d961e95
Henri K <12832691+nkahe@users.noreply.github.com>
2025-10-30 17:36:46 +0200

    Add Snacks

--- lua/config/20_keymaps.lua
+++ lua/config/20_keymaps.lua
@@ -1,8 +1,7 @@
 local map = vim.keymap.set
 
-map("n", "<leader>e", "<cmd>Explore<cr>", { desc = "Explore" })
 
-map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy.nvim" })
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy" })
 
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 map("n", "<leader>qQ", "<cmd>qa!<cr>", { desc = "Quit All without saving" })
