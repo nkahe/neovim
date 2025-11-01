110b669c43e965aed02a9846278f48275a78bd6f
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-01 22:32:24 +0200

    Add more Lua LSP configuration, Neogit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,31 +4,75 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "mason-org/mason.nvim",
       {
-        "mason-org/mason-lspconfig.nvim",
+        -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+        -- used for completion, annotations and signatures of Neovim apis.
+        "folke/lazydev.nvim",
+        ft = "lua",
         opts = {
-          ensure_installed = { "bashls", "lua_ls" },
+          library = {
+            -- Load luvit types when the `vim.uv` word is found
+            -- See the configuration section for more details
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
-      "nvim-mini/mini.nvim",
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+      "nvim-mini/mini.nvim"  -- So mini.completion is available.
     },
-    opts = {
-      servers = { "bashls", "lua_ls" },
-      capabilities = function()
-        local MiniCompletion = require("mini.completion")
-        return MiniCompletion.get_lsp_capabilities()
-      end,
+    -- opts = {
+    --   servers = { "bashls", "lua_ls" },
       -- on_attach = function(client, bufnr)
       --   vim.bo[bufnr].omnifunc = "v:lua.MiniCompletion.completefunc_lsp",
       --   vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
       -- end
-    },
-    config = function(_, opts)
+    -- },
+    config = function()
+
+      -- Don't enable LSP if using Obsidian.nvim.
+      if vim.g.obsidian then
+        return
+      end
+
+      -- require('lspconfig').lua_ls.setup({
+      --   settings = {
+      --     Lua = {
+      --       runtime = {
+      --         -- Tell the language server you're using LuaJIT in Neovim
+      --         version = 'LuaJIT',
+      --       },
+      --       diagnostics = {
+      --         -- Recognize the `vim` global
+      --         globals = { 'vim' },
+      --       },
+      --       workspace = {
+      --         -- Make the server aware of Neovim runtime files
+      --         library = vim.api.nvim_get_runtime_file("", true),
+      --         checkThirdParty = false,
+      --       },
+      --       telemetry = { enable = false },
+      --     },
+      --   },
+      -- })
+
+      -- capabilities = function()
+      --   local MiniCompletion = require("mini.completion")
+      --   return MiniCompletion.get_lsp_capabilities()
+      -- end
+
+      local MiniCompletion = require("mini.completion")
+      local capabilities = MiniCompletion.get_lsp_capabilities()
+
       -- Configure Mason LSP after it loads
-      local mlsp = require("mason-lspconfig")
-      mlsp.setup()  -- basic setup
-      local lspconfig = require("lspconfig")
+      -- local mlsp = require("mason-lspconfig")
+      -- mlsp.setup()  -- basic setup
+
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      vim.lsp.enable({ "lua_ls", "bashls", "shellcheck" })
+
+      -- Testaa: vim.api.
 
       -- manually setup servers listed in opts.servers
       -- for _, server in ipairs(opts.servers) do
@@ -41,14 +85,12 @@ return {
       --     })
       --   end
       -- end
-
-    end,
+    end
   },
-
   {
     "mason-org/mason.nvim",
     opts = {
-      ensure_installed = { "shellcheck", "bashls", "lua_ls" },
+      ensure_installed = { "bashls", "lua_ls", "shellcheck" },
       install_root_dir = vim.fn.expand("~/.local/share/nvim/mason"),
     },
   },
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,32 +3,53 @@
 
 -- mini.nvim. https://nvim-mini.org/mini.nvim
 return {
-  'nvim-mini/mini.nvim', version = false,
-  config = function()
-    require('mini.basics').setup({
-      -- Manage options in 'plugin/10_options.lua'
-      options = {
-        -- Basic options ('number', 'ignorecase', and many more)
-        basic = false,
-        -- Extra UI features ('winblend', 'listchars', 'pumheight', ...)
-        extra_ui = true,
-       -- Presets for window borders ('single', 'double', ...)
-        -- Default 'auto' infers from 'winborder' option
-        win_borders = 'auto',
-      },
-      mappings = {
-        basic = true,
-        -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
-        -- Supply empty string to not create these mappings.
-        -- Defined in keymaps file.
-        option_toggle_prefix = '',
-        -- Create `<C-hjkl>` mappings for window navigation
-        windows = true,
-        -- Create `<M-hjkl>` mappings for navigation in Insert and Command modes
-        move_with_alt = true,
-      },
-    })
-    require('mini.move').setup()
-    require('mini.sessions').setup()
-  end,
+  {
+    'nvim-mini/mini.nvim', version = false,
+    config = function()
+      require('mini.basics').setup({
+        -- Manage options in 'plugin/10_options.lua'
+        options = {
+          -- Basic options ('number', 'ignorecase', and many more)
+          basic = false,
+          -- Extra UI features ('winblend', 'listchars', 'pumheight', ...)
+          extra_ui = true,
+          -- Presets for window borders ('single', 'double', ...)
+          -- Default 'auto' infers from 'winborder' option
+          win_borders = 'auto',
+        },
+        mappings = {
+          basic = true,
+          -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
+          -- Supply empty string to not create these mappings.
+          -- Defined in keymaps file.
+          option_toggle_prefix = '',
+          -- Create `<C-hjkl>` mappings for window navigation
+          windows = true,
+          -- Create `<M-hjkl>` mappings for navigation in Insert and Command modes
+          move_with_alt = true,
+        },
+      })
+      require('mini.move').setup()
+      require('mini.sessions').setup()
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      -- "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+      -- "nvim-mini/mini.pick",           -- optional
+      "folke/snacks.nvim",                -- optional
+    },
+    opts = {
+      graph_style = "unicode", -- or "ascii", "none"
+    },
+    keys = {
+      { "<Leader>gg", "<cmd>Neogit kind=floating<CR>",  mode = "n", desc = 'Toggle Neogit' },
+    }
+  }
 }
