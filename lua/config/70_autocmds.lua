084533e66dea69844f58b8f3edec82e0fe181c92
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-01 11:37:23 +0200

    Close some file types with q.

--- lua/config/70_autocmds.lua
+++ lua/config/70_autocmds.lua
@@ -1,7 +1,44 @@
 
+-- Set title
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     local filename = vim.fn.expand("%:t") ~= "" and vim.fn.expand("%:t") or "[No Name]"
     vim.o.titlestring = "Custom - " .. filename
   end,
 })
+
+-- close some filetypes with <q>. From Lazyvim.
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
+})
+
