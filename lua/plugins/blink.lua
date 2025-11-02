5ef5b592d961e744e9c89bdc1488b1d1a9c4188a
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-02 16:24:11 +0200

    Add Blink config

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,99 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  -- dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+  event = { "InsertEnter", "CmdlineEnter" },
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'super-tab' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- Show the documentation popup automatically.
+    completion = {
+      -- documentation = {
+      --   auto_show = true,
+      --   auto_show_delay_ms = 200,
+      -- },
+      -- Display a preview of the selected item on the current line
+      ghost_text = {
+        enabled = false,
+      },
+      -- Don't insert completion item on selection, don't select by default
+      -- list = {
+      --   selection = {
+      --     preselect = true,
+      --     auto_insert = false
+      --   }
+      -- },
+    },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" },
+
+  -- TODO: Convert to regular keymaps?
+  keymap = {
+    -- Gave error for some reason, so mappings are set manually below.
+    -- preset = "super-tab"
+    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    ['<C-e>'] = { 'hide', 'fallback' },
+    -- From blink documentation.
+    ['<Tab>'] = {
+      function(cmp)
+        if cmp.snippet_active() then return cmp.accept()
+        else return cmp.select_and_accept() end
+      end,
+      'snippet_forward',
+      'fallback'
+    },
+    ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+    ['<Up>'] = { 'select_prev', 'fallback' },
+    ['<Down>'] = { 'select_next', 'fallback' },
+    ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+    ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+  },
+
+}
