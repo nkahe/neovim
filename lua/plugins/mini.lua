0672c7630fb49adc70c4d8a9b750f6e96d3e021c
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-03 17:14:10 +0200

    Arrange plugin files

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,76 @@
+
+-- Misc plugins
+
+-- mini.nvim. https://nvim-mini.org/mini.nvim
+return {
+  'nvim-mini/mini.nvim', version = false,
+  event = 'VeryLazy',
+  config = function()
+    require('mini.basics').setup({
+      -- Manage options in 'plugin/10_options.lua'
+      options = {
+        -- Basic options ('number', 'ignorecase', and many more)
+        basic = false,
+        -- Extra UI features ('winblend', 'listchars', 'pumheight', ...)
+        extra_ui = true,
+        -- Presets for window borders ('single', 'double', ...)
+        -- Default 'auto' infers from 'winborder' option
+        win_borders = 'auto',
+      },
+      mappings = {
+        basic = true,
+        -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
+        -- Supply empty string to not create these mappings.
+        -- Defined in keymaps file.
+        option_toggle_prefix = '',
+        -- Create `<C-hjkl>` mappings for window navigation
+        windows = true,
+        -- Create `<M-hjkl>` mappings for navigation in Insert and Command modes
+        move_with_alt = true,
+      },
+    })
+
+    -- Extra 'mini.nvim' functionality.
+    require('mini.extra').setup()
+
+    local ai = require('mini.ai')
+    ai.setup({
+      -- 'mini.ai' can be extended with custom textobjects.
+      custom_textobjects = {
+        -- Make `aB` / `iB` act on around/inside whole *b*uffer
+        B = MiniExtra.gen_ai_spec.buffer(),
+        -- For more complicated textobjects that require structural awareness,
+        -- use tree-sitter. This example makes `aF`/`iF` mean around/inside function
+        -- definition (not call). See `:h MiniAi.gen_spec.treesitter()` for details.
+        F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+      },
+
+      -- 'mini.ai' by default mostly mimics built-in search behavior: first try
+      -- to find textobject covering cursor, then try to find to the right.
+      -- Although this works in most cases, some are confusing. It is more robust to
+      -- always try to search only covering textobject and explicitly ask to search
+      -- for next (`an`/`in`) or last (`an`/`il`).
+      -- Try this. If you don't like it - delete next line and this comment.
+      search_method = 'cover',
+    })
+
+    local hipatterns = require('mini.hipatterns')
+    local hi_words = MiniExtra.gen_highlighter.words
+    hipatterns.setup({
+      highlighters = {
+        -- Highlight a fixed set of common words. Will be highlighted in any place,
+        -- not like "only in comments".
+        fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+        hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+        todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+        note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+
+        -- Highlight hex color string (#aabbcc) with that color as a background
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    })
+
+    require('mini.move').setup()
+    require('mini.sessions').setup()
+  end,
+}
