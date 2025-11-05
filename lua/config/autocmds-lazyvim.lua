fa65aadb1ffb394a13f3bfaef1e144b59c3d0c90
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-06 00:50:41 +0200

    More autocmds from lazyvim

--- lua/config/autocmds-lazyvim.lua
+++ lua/config/autocmds-lazyvim.lua
@@ -7,6 +7,26 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("Custom_" .. name, { clear = true })
 end
 
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
 -- go to last loc when opening a buffer.
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup("last_loc"),
@@ -59,6 +79,26 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+-- wrap in text filetypes. Narkdown has ftplugin.
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap"),
+  pattern = { "text", "plaintex", "typst", "gitcommit" },
+  callback = function()
+    vim.opt_local.wrap = true
+    -- vim.opt_local.spell = true
+  end,
+})
+
 
 -- Fix conceallevel for json files.
 vim.api.nvim_create_autocmd({ "FileType" }, {
