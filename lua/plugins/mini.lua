51d6303f7e8f4b1a6f096417770bec8f61be6bc7
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-09 15:38:54 +0200

    Mini.highlight: words need ":"

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -148,16 +148,14 @@ return {
       highlighters = {
         -- Highlight a fixed set of common words. Will be highlighted in any place,
         -- not like "only in comments".
-        fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
-        hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
-        todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
-        note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
-
+        fixme = hi_words({ 'FIXME:', 'Fixme:', 'fixme:' }, 'MiniHipatternsFixme'),
+        hack = hi_words({ 'HACK:', 'Hack:', 'hack:' }, 'MiniHipatternsHack'),
+        todo = hi_words({ 'TODO:', 'Todo:', 'todo:' }, 'MiniHipatternsTodo'),
+        note = hi_words({ 'NOTE:', 'Note:', 'note:' }, 'MiniHipatternsNote'),
         -- Highlight hex color string (#aabbcc) with that color as a background
         hex_color = hipatterns.gen_highlighter.hex_color(),
       },
     })
-
     -- Move text blocks easily.
     require('mini.move').setup()
 
