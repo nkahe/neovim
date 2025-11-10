95075b944f8ddacb5b6a4500d549ce664380626f
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-10 09:10:59 +0200

    More bufferline settings + keymaps

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,21 +1,63 @@
 
 -- UI related plugins.
 
+-- Configs are mostly from LazyVim.
 return {
 
--- Bufferline -----------------------------------------------------------------
+-- Bufferline
   {
     'akinsho/bufferline.nvim',
     enabled = true,
-    version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
     opts = {
+
       options = {
+        -- stylua: ignore
+        close_command = function(n) Snacks.bufdelete(n) end,
         always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+          {
+            filetype = "snacks_layout_box",
+          },
+        },
       }
     },
+
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+
   },
 
--- Lualine --------------------------------------------------------------------
+-- Lualine
   {
     "nvim-lualine/lualine.nvim",
     enabled = true,
