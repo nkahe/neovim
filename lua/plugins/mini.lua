1a1f6d22abea788d4da1ae596066f2a3f11214b0
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-13 14:39:17 +0200

    mini.ai: add entire buffer object

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,12 +34,31 @@ return {
     -- Extra 'mini.nvim' functionality.
     require('mini.extra').setup()
 
+    -- Entire buffer object.
+    local function buffer(ai_type)
+      local start_line, end_line = 1, vim.fn.line("$")
+      if ai_type == "i" then
+        -- Skip first and last blank lines for `i` textobject
+        local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+        -- Do nothing for buffer with all blanks
+        if first_nonblank == 0 or last_nonblank == 0 then
+          return { from = { line = start_line, col = 1 } }
+        end
+        start_line, end_line = first_nonblank, last_nonblank
+      end
+
+      local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+      return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+    end
+
+
    -- Extends the a & i text objects, this adds the ability to select
     -- arguments, function calls, text within quotes and brackets, and to
     -- repeat those selections to select an outer text object.
     -- This is based on LazyVim config.
     local ai = require('mini.ai')
     ai.setup({
+
       n_lines = 500,
       custom_textobjects = {
         o = ai.gen_spec.treesitter({ -- code block
@@ -54,6 +73,7 @@ return {
           { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
           "^().*()$",
         },
+        g = buffer,
         u = ai.gen_spec.function_call(), -- u for "Usage"
         U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
       },
