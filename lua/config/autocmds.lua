7693afeb0a67245f1de14fb4810d106bcb2f6e25
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-13 14:38:22 +0200

    Close window when terminal terminates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -35,18 +35,42 @@ vim.api.nvim_create_autocmd({ "BufEnter", "TermClose" }, {
 
 -- Set black background color for terminal and start in insert mode.
 -- Color doesn't work always.
-vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+vim.api.nvim_create_autocmd({ "TermOpen", "WinEnter" }, {
   group = augroup("set_terminal_settings"),
   pattern = "*",
   callback = function()
      if vim.bo.buftype == "terminal" then
-      -- These custom groups are set in coloscheme config.
-      -- vim.api.nvim_set_hl(0, "TermBackground", { bg = "#121212" })
-      -- vim.api.nvim_set_hl(0, "TermCursorLine", { bg = "none" })
-      vim.opt_local.winhighlight = "Normal:TermBackground,CursorLine:TermCursorLine"
       vim.cmd("startinsert")
       local prefix = (_G.Config and _G.Config.windowtitle) or "Neovim"
       vim.o.titlestring = prefix .. " - terminal"
+    end
+  end,
+})
+
+-- Adding "VimResized" didn't have an effect.
+vim.api.nvim_create_autocmd({ "TermOpen", "WinEnter"  }, {
+  group = augroup("set_terminal_background_color"),
+  pattern = "*",
+  callback = function()
+      -- These custom groups are set in coloscheme config.
+      -- vim.api.nvim_set_hl(0, "TermBackground", { bg = "#121212" })
+      -- vim.api.nvim_set_hl(0, "TermCursorLine", { bg = "none" })
+    if vim.bo.buftype == "terminal" or vim.bo.filetype == "snacks_terminal" then
+      vim.opt_local.winhighlight = "Normal:TermBackground,CursorLine:TermCursorLine"
+    end
+  end,
+})
+
+-- When exiting terminal shell, just close window and don't print
+-- [Process exited 130] and wait for a keypress.
+vim.api.nvim_create_autocmd("TermClose", {
+  callback = function(args)
+    local win = vim.fn.bufwinid(args.buf)
+    if win ~= -1 then
+      local ok, _ = pcall(vim.api.nvim_win_close, win, true)
+      if not ok then
+        -- fallback: maybe it was already gone
+      end
     end
   end,
 })
