c3f4085146577ee50cbf346f98db96b74213cf15
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-15 18:11:57 +0200

    Noice: no auto signature or window borders

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,6 @@
 return {
   "folke/noice.nvim",
-    enabled = true,
+  enabled = true,
   event = "VeryLazy",
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -9,26 +9,41 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     -- "rcarriga/nvim-notify",
-    },
+  },
   opts = {
     notify = {
       enabled = true,
       timeout = 10000,
     },
+
+    lsp ={
+      signature = {
+        enabled = true,
+        auto_open = {
+          -- Ctrl-k for hiding signature window doesn't work for me but moved
+          -- the cursor to it.
+          enabled = false,
+          -- trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+          -- luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+          -- throttle = 50, -- Debounce lsp signature help request by 50ms
+        },
+      },
+    },
     -- mini = {
     --   timeout = 10000
     -- },
-  routes = {
-    -- hide "Search hit BOTTOM/TOP" warnings
-    {
-      filter = {
-        -- "Search hit bottom" type of warnings.
-        event = "msg_show",
-        kind = "wmsg",
+    routes = {
+      -- hide "Search hit BOTTOM/TOP" warnings
+      {
+        filter = {
+          -- "Search hit bottom" type of warnings.
+          event = "msg_show",
+          kind = "wmsg",
+        },
+        opts = { skip = true },
       },
-      opts = { skip = true },
     },
-  },
+
     cmdline = {
       format = {
         -- These disable hiding of matching command.
@@ -40,11 +55,6 @@ return {
         help = false
       },
       view = "cmdline_popup", -- Use the popup view
-    },
-
-    presets = {
-      long_message_to_split = true, -- long messages will be sent to a split
-      lsp_doc_border = false, -- add a border to hover docs and signature help
     },
 
     views = {
@@ -70,5 +80,10 @@ return {
       },
     },
 
-  },
+    presets = {
+      long_message_to_split = true, -- long messages will be sent to a split
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+
+  }, --opts
 }
