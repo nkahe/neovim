2bc647f5b21f0b915c07ac982f747df9ce4171d9
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-24 18:38:50 +0200

    Tweak base46 theme settings.

--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -1,47 +1,57 @@
+
 local options = {
 
   -- NOTE: After changing theme, run:
   -- lua require("base46").compile()
   -- lua require("base46").load_all_highlights()
+
   base46 = {
     theme = "oceanic-next", -- default theme
     hl_add = {
-      -- Needed to add atleast these.
-      NoiceCmdlinePopupBorder = { fg = "teal" },
+      NoiceCmdlinePopupBorder       = { fg = "teal" },
       NoiceCmdlinePopupBorderSearch = { fg = "teal" },
-      SnacksPickerListCursorne = { bg = "one_bg2" },
+      SnacksPickerListCursorline    = { bg = "one_bg" },
+      SnacksPickerCursorline        = { bg = "one_bg" },
       SnacksPickerDir = { fg = "nord_blue" },
-      TermBackground = { bg = "#0b1216" },
-      TermCursorLine = { bg = "none" }
+      SnacksIndent    = { fg = "one_bg2" },
+      -- Custom groups used in autocmd.
+      TermBackground  = { bg = "#0b1216" },
+      TermCursorLine  = { bg = "none" }
     },
     hl_override = {
-      Comment = { fg = "teal" },
-      ["@comment"] = { fg = "teal", },
-      IncSearch = { fg = "black", bg = 'teal' },
+      Comment   = { fg = "light_grey" },
+      -- Make text in search hilights easier to read.
+      IncSearch = { fg = "#000000" },
+      Search    = { fg = "#000000" },
       WhichKeyDesc = { fg = "blue" },
+      -- Treesitter
+      -- Use lighter grey.
+      ["@comment"] = { fg = "light_grey", },
+      -- Use similar color as original Sublime Text Oceanic-Next instead
+      -- of Base46 definition which is linked to red.
+      ["@punctuation.delimiter"] = { fg = 'base08' },
+      ["@punctuation.bracket"]   = { fg = 'base08' },
+      ["@constructor"] = { fg = 'base08' },
     },
-    integrations = {
-      -- Needed to add atleast these.
-      NoiceCmdlinePopupBorder = { fg = "teal" },
-      NoiceCmdlinePopupBorderSearch = { fg = "teal" },
-      SnacksPickerListCursorLine = { bg = "one_bg2" },
-      SnacksPickerDir = { fg = "nord_blue" },
-  },
+    integrations = { "blink", "flash", "git", "git-conflict", "lsp", "mason",
+      "neogit", "semantic_tokens", "syntax", "treesitter", "trouble", "whichkey"
+    },
 -- NOTE: changed_themes allows only custom hex colors
     changed_themes = {
       ["oceanic-next"] = {
         base_30 = {
-          black = "#192830",
+          black  = "#192830", -- Make little darker.
           black2 = "#203038",
+          -- Make lighter, used in comments.
+          light_grey = "#738490" 
         },
         base_16 = {
           base00 = "#192830",
         },
         -- These weren't enough.
-        NoiceCmdlinePopupBorder = { fg = "#50a4a4" },
+        NoiceCmdlinePopupBorder       = { fg = "#50a4a4" },
         NoiceCmdlinePopupBorderSearch = { fg = "#50a4a4" },
-        SnacksPickerListCursorLine = { bg = "#2e3e47" },
-        SnacksPickerDir = { fg = "#598cbf" },
+        SnacksPickerListCursorLine    = { bg = "#2e3e47" },
       },
     },
     transparency = false,
@@ -90,3 +100,59 @@ local options = {
 
 local status, chadrc = pcall(require, "chadrc")
 return vim.tbl_deep_extend("force", options, status and chadrc or {})
+
+-- For reference
+
+-- M.base_30 = {
+--   white = "#D8DEE9", -- confirmed
+--   darker_black = "#122027",
+--   black = "#1B2B34", --  nvim bg
+--   black2 = "#21313a",
+--   one_bg = "#25353e",
+--   one_bg2 = "#2e3e47",
+--   one_bg3 = "#36464f",
+--   grey = "#43535c",
+--   grey_fg = "#4d5d66",
+--   grey_fg2 = "#576770",
+--   light_grey = "#5f6f78",
+--   red = "#EC5F67",
+--   baby_pink = "#ff7d85",
+--   pink = "#ffafb7",
+--   line = "#2a3a43", -- for lines like vertsplit
+--   green = "#99C794",
+--   vibrant_green = "#b9e75b",
+--   nord_blue = "#598cbf",
+--   blue = "#6699CC",
+--   yellow = "#FAC863",
+--   sun = "#ffd06b",
+--   purple = "#C594C5",
+--   dark_purple = "#ac7bac",
+--   teal = "#50a4a4",
+--   orange = "#F99157",
+--   cyan = "#62B3B2",
+--   statusline_bg = "#1f2f38",
+--   lightbg = "#2c3c45",
+--   pmenu_bg = "#15bf84",
+--   folder_bg = "#598cbf",
+-- }
+
+-- Base16 colors taken from:
+-- M.base_16 = {
+--   base00 = "#1B2B34",
+--   base01 = "#343D46",
+--   base02 = "#4F5B66",
+--   base03 = "#65737e",
+--   base04 = "#A7ADBa",
+--   base05 = "#C0C5Ce",
+--   base06 = "#CDD3De",
+--   base07 = "#D8DEE9",
+--   base08 = "#6cbdbc",
+--   base09 = "#FAC863",
+--   base0A = "#F99157",
+--   base0B = "#99C794",
+--   base0C = "#5aaeae",
+--   base0D = "#6699CC",
+--   base0E = "#C594C5",
+--   base0F = "#EC5F67",
+-- }
+
