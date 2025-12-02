825301a452c6ab20a3cd8ed812b7a67cddb16f68
Henri K <12832691+nkahe@users.noreply.github.com>
2025-12-02 16:12:32 +0200

    Some Oil keymaps

---
+++ after/ftplugin/oil.lua
@@ -0,0 +1,8 @@
+
+-- For Oil file explorer.
+
+vim.opt_local.colorcolumn = ""
+
+-- Remap these so they work for buffer change.
+vim.keymap.set("n", "H", "H", { buffer = true })
+vim.keymap.set("n", "L", "L", { buffer = true })
