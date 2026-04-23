0cbd56d39a539cca09d9fa2b72e2c969c7554e0a
Henri K <12832691+nkahe@users.noreply.github.com>
2026-04-23 19:06:10 +0300

    Theme: snacks.notifier

--- lua/plugins/local/neoceanic/lua/neoceanic/groups/snacks.lua
+++ lua/plugins/local/neoceanic/lua/neoceanic/groups/snacks.lua
@@ -1,12 +1,19 @@
 local M = {}
 
-function M.get(_, colors, _)
-  return {
-    SnacksPickerListCursorline = { bg = colors.one_bg },
-    SnacksPickerCursorline     = { bg = colors.one_bg },
-    SnacksPickerDir            = { fg = colors.nord_blue },
-    SnacksIndent               = { fg = colors.one_bg2 },
-  }
-end
+function M.get(_, c, _)
+  local border_c = c.grey_fg
+    return {
+      SnacksNotifierBorderDebug  = { fg = border_c },
+      SnacksNotifierBorderError  = { fg = border_c },
+      SnacksNotifierBorderInfo   = { fg = border_c },
+      SnacksNotifierBorderWarn   = { fg = border_c },
+
+      SnacksPickerListCursorline = { bg = c.one_bg },
+      SnacksPickerCursorline     = { bg = c.one_bg },
+      SnacksPickerDir            = { fg = c.nord_blue },
+
+      SnacksIndent               = { fg = c.one_bg2 },
+    }
+  end
 
 return M
