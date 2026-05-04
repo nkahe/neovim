b7ad2491b3ace0415aee34630fbf66261372c514
Henri K <12832691+nkahe@users.noreply.github.com>
2026-05-04 19:16:20 +0300

    Neogit deps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -100,7 +100,6 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
       -- "dlyongemallo/diffview.nvim",        -- optional - Diff integration
       "esmuellert/codediff.nvim",      -- optional
 
