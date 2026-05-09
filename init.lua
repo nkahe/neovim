fa0e56bd103e506c2fac2c99a4139e3f118b460a
Henri K <12832691+nkahe@users.noreply.github.com>
2026-05-09 13:46:22 +0300

    Rm old base46 config

--- init.lua
+++ init.lua
@@ -7,17 +7,7 @@ _G.Config.windowtitle = 'Custom'
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
- -- base46 themes: put this in your main init.lua file ( before lazy setup )
--- vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
-
 -- Define custom autocommand group and helper to create an autocommand.
--- Autocommands are Neovim's way to define actions that are executed on events
--- (like creating a buffer, setting an option, etc.).
---
--- See also:
--- - `:h autocommand`
--- - `:h nvim_create_augroup()`
--- - `:h nvim_create_autocmd()`
 local gr = vim.api.nvim_create_augroup('custom-config', {})
 _G.Config.new_autocmd = function(event, pattern, callback, desc)
   local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
@@ -26,12 +16,6 @@ end
 -- Source config files.
 
 require("config.lazy")
--- put this after lazy setup 
-
--- (method 1, For heavy lazyloaders)
- -- dofile(vim.g.base46_cache .. "defaults")
- -- dofile(vim.g.base46_cache .. "statusline")
-
 require("config.options")
 require("config.keymaps")
 require("config.shared-keymaps")
@@ -40,11 +24,6 @@ require("config.shared-keymaps")
 if vim.g.vscode then
   require("config.vscode")
 else
-  -- (method 2, for non lazyloaders) to load all highlights at once
-  -- for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-  --   dofile(vim.g.base46_cache .. v)
-  -- end
-
   require("config.autocmds-lazyvim")
   require("config.autocmds")
   require("config.commands")
