3aa660b02345815ca336dd2276755713bb97f3ff
Henri K <12832691+nkahe@users.noreply.github.com>
2026-08-14 16:02:14 +0300

    Markdown-plus: enable list management due new features

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -110,12 +110,14 @@ return {
   -- provides modern markdown editing capabilities, implementing features found
   -- in popular editors.
   -- https://github.com/yousefhadder/markdown-plus.nvim?tab=readme-ov-file
-  -- Use with '\' + 
   {
     "yousefhadder/markdown-plus.nvim",
     enabled = true,
     ft = "markdown",
     opts = {
+      features = {
+        list_management = true,
+      },
       table = {
         keymaps = { prefix = "<Leader>T" }
       }
